fa532cf3219d3a9f750a20fa6fb5f1032435581d
FelicityWivi <FelicityRepudiation@gmail.com>
2026-02-14 17:28:38 +0000

    fix

--- Field_Effect V2.0.0/Battle functions/[007]_Other battle code/001_PBEffects.rb
+++
@@ -1,9 +0,0 @@
-module PBEffects
-  #===========================================================================
-  # These effects apply to a battler
-  #===========================================================================
-  #Repud Effects start from 1000 in-case of overlapping with plugins
-  Shelter             = 1001
-  CaveCollapse    = 913
-  CollapseWarning = 914
-end
